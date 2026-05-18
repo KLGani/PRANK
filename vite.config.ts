b5cc912b9bf086e1eaa854c31b7917f5b209749e
KLGani <klgani19@gmail.com>
2026-05-19 02:25:05 +0530

    Reorganize imports and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  preview: {
-    allowedHosts: ['.onrender.com', 'edu-predict-nwcq.onrender.com']
-  }
-})
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    viteSingleFile()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  preview: {
+    allowedHosts: ['.onrender.com', 'edu-predict-nwcq.onrender.com']
+  }
 });
